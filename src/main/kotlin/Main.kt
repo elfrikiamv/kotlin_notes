f58ceb90135b8ff8d946a3cb57478efab1268aa0
elfrikiamv <elfrikiamv@gmail.com>
2022-04-24 18:22:58 -0500

    Variables.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,80 @@
-//inicio git
+//inicio git.
+
+    //-------------> Las constantes no deben cambiar nunca y se generan antes de darle compilar la aplicación.
+    const val my_age = 22
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    //println("Mi edad es: "+my_age)
+    println("Mi edad es: $my_age años")
+
+    //-------------> Variable de lectura y escritura.
+
+    var saldo = 13
+    //println("Saldo actual: "+saldo)
+    println("Tu saldo actual es de: $saldo pejecoins")
+    saldo = 42
+    //println("Saldo actualizado: "+saldo)
+    println("Tu saldo se a actualizado: $saldo pejecoins")
+
+    //-------------> variable de solo lectura (No se puede modificar).
+
+    val name = "M&j"
+    //println("Su nombre es: "+name)
+    println("Su nombre es: $name")
+
+    //-------------> kotlin detecta automaticamente el tipo de variable pero si eres necio lo puedes especificar:
+
+    //Típos numéricos.
+
+    //BYTE
+    val numerobyte : Byte = 127
+    println("Byte: $numerobyte")
+
+    //SHORT
+    val numeroshort : Short = 32767
+    println("Short: $numeroshort")
+
+    //INT
+    val numeroInt : Int = 2147483647
+    println("Int: $numeroInt")
+
+    //LONG
+    val numeroLong : Long = 9223372036854775807
+    val numerol = -9223372036854775807L
+    println("Long: $numeroLong\nLong: $numerol")
+
+    //DOUBLE
+    val numeroDouble : Double = 18.01234567890123
+    println("Double: $numeroDouble")
+
+    //FLOAT
+    val numeroFloat : Float = 8.012345f
+    println("Float: $numeroFloat")
+
+    //Típos alfanuméricos.
+
+    //BOOLEAN
+    val booleanVerdadero : Boolean = true
+    val booleanFalso : Boolean = false
+    println("Boolean: $booleanVerdadero\nBoolean: $booleanFalso")
+
+    //CHAR
+    val caracter : Char = 'F'
+    val caracterNumero : Char = '2'
+    val caracterChar : Char = '@'
+    println("Char: $caracter\nChar: $caracterNumero\nChar: $caracterChar")
+
+    //STRING
+    val textString : String  = "Hola, soy un string xd"
+    val testString : String = "Test. 12345!-$%S/"
+    println("String: $textString\nString: $testString")
+
+
+}
+/*
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
-}
+ */
